b848ae31d24ab81947243e08e14621c8ff5881f0
Chris Marshall <chris@chrismar035.com>
2015-12-14 22:51:14 -0500

    Load sudoku puzzles into redis from a source file

---
+++ README.md
@@ -0,0 +1,4 @@
+# sudoku-loader
+
+Loads Sudoku puzzles into a redis set from an input file. The file, 
+`puzzles.txt`, has lines of 81 integers between 0 and 9. 
---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"crypto/sha1"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/chrismar035/solver"
+
+	"gopkg.in/redis.v3"
+)
+
+type Sudoku struct {
+	Id       string  `json:"id"`
+	Puzzle   [81]int `json:"puzzle"`
+	Solution [81]int `json:"solution"`
+	Name     string  `json:"name"`
+}
+
+func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_ADDR"),
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	_, err := client.Ping().Result()
+	if err != nil {
+		panic(err)
+	}
+
+	key := "puzzles"
+
+	file, err := os.Open("puzzles.txt")
+	if err != nil {
+		fmt.Println("Error opening puzzles", err)
+	}
+	scanner := bufio.NewScanner(file)
+	lineNumber := 0
+	for scanner.Scan() {
+		lineNumber++
+		line := scanner.Text()
+		if len(line) != 81 {
+			fmt.Println("Puzzle improperly sized on line", line, err)
+		}
+
+		var source bytes.Buffer
+		var grid solver.Grid
+		for i, char := range strings.Split(line, "") {
+			source.WriteString(char)
+			grid[i], err = strconv.Atoi(char)
+			if err != nil {
+				fmt.Println("Invalid puzzle line", lineNumber, "char", i)
+			}
+		}
+
+		puzzle := solver.Puzzle{Initial: grid}
+		solver := solver.NewSolver()
+		puzzle.Solution = solver.Solve(grid)
+
+		for _, number := range puzzle.Solution {
+			source.WriteByte(byte(number))
+		}
+		hasher := sha1.New()
+		hasher.Write(source.Bytes())
+		sha := hex.EncodeToString(hasher.Sum(nil))
+
+		sudoku := Sudoku{Id: sha, Puzzle: puzzle.Initial, Solution: puzzle.Solution}
+
+		var contentBuf bytes.Buffer
+		if err = json.NewEncoder(&contentBuf).Encode(sudoku); err != nil {
+			panic(err)
+		}
+		client.SAdd(key, contentBuf.String())
+		fmt.Println("Added", key, sha, contentBuf.String())
+	}
+}
